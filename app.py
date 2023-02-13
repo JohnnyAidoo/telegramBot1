7581fe0bf116ad09cb6de37885cfb996b45d519f
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-02-13 22:02:38 +0000

    'ok'

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ token ='6293141367:AAForYshseRqGS7AImNjCJjOg0sXbXIA3yw'
 def start(update, context):
     update.message.reply_text('welcome')
 
+def help(update , context):
+    update.message.reply_text("""
+    This is my test bot. Created by Johnny Osei Aidoo.
+    use
+    /start => to start the bot
+    """)
+
 def handleText(update, context):
     update.message.reply_text(f"You said {update.message.text}")
 
@@ -16,6 +23,10 @@ def main():
     
     start_handler = CommandHandler("start", start)
     dp.add_handler(start_handler)
+    help_handler = CommandHandler('help', help)
+    dp.add_handler(help_handler)
+
+
     dp.add_handler(MessageHandler(Filters.text, handleText))
 
     updater.start_polling()
